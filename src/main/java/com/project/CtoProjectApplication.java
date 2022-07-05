618784aa4dced8b5e57d830297ca32a8a4e618ea
jmpark <https://lab.hanium.or.kr/22_IG031/22_ig031.git>
2022-07-05 14:58:02 +0900

    cto-project

---
+++ src/main/java/com/project/CtoProjectApplication.java
@@ -0,0 +1,21 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CtoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtoProjectApplication.class, args);
+	}
+
+	@GetMapping
+	public String HelloWorld() {
+		return "HelloWorld";
+	}
+
+}

260a9a4e4608dd60e3856aecb4f6839e53620788
lmj <123dkan@naver.com>
2022-07-07 19:15:18 +0900

    [imj] 헤더, 푸터 고정 스크립트 추가/ sub_00_b.html 파일에 적용 테스트 완료/ 기타 html 파일 언어 ko로 변경

---
+++ src/main/resources/templates/includeHtml.js
@@ -0,0 +1,24 @@
+/*
+    TODO: 참고 사이트 ( 이해 불가 )
+    1. https://kyung-a.tistory.com/18
+    2. https://www.w3schools.com/howto/howto_html_include.asp
+    또는 해당 파일에 스크립트 태그 넣고 코드를 넣는 방법도 있지만, 적용해야할 파일이 너무 많아서 js 파일 따로 분리해놓음
+    해당 파일명: includeHtml.js
+*/
+
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});

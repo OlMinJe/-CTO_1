0b520e3e3a460326f329e450e3e413675d058ac7
lmj <123dkan@naver.com>
2022-07-09 11:36:08 +0900

    [imj] mypage 개어렵다

--- src/main/resources/static/js/mypage/mypage.js
+++ src/main/resources/static/js/mypage/mypage.js
@@ -1,10 +1,6 @@
 /* TODO: (참고 사이트) https://seongeun-it.tistory.com/113 */
 function showMenu(element){
-    document.getElementsByClassName("content_box_main")[0].style.display = "none";
-        console.log("test");
-
-
-   var content = document.getElementsByClassName("content_box");
+    var content = document.getElementsByClassName("content_box");
     for(var i=0; i<content.length; i++){
         if(element.id+"_content" == content[i].id){
             content[i].style.display = "block";
@@ -13,3 +9,126 @@ function showMenu(element){
         }
     }
 }
+/* TOdO: (참고 사이트) https://velog.io/@minkyeong-ko/HTMLCSSJS-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C-%ED%8C%8C%EC%9D%BC%EC%9D%B4%EB%A6%84-%EB%82%98%ED%83%80%EB%82%B4%EA%B8%B0-%ED%99%94%EB%A9%B4%EC%97%90-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EB%B3%B4%EC%97%AC%EC%A3%BC%EA%B8%B0 */
+var submit = document.getElementById('submitButton');
+submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
+
+function showImage() {
+    var newImage = document.getElementById('image-show').lastElementChild;
+    newImage.style.visibility = "visible";
+
+    document.getElementById('image-upload').style.visibility = 'hidden';
+
+    document.getElementById('fileName').textContent = null;     //기존 파일 이름 지우기
+}
+
+function loadFile(input) {
+    var file = input.files[0];
+
+    var name = document.getElementById('fileName');
+    name.textContent = file.name;
+
+    var newImage = document.createElement("img");
+    newImage.setAttribute("class", 'img');
+
+    newImage.src = URL.createObjectURL(file);
+
+    newImage.style.width = "70%";
+    newImage.style.height = "70%";
+    newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
+    newImage.style.objectFit = "contain";
+
+    var container = document.getElementById('image-show');
+    container.appendChild(newImage);
+}
+
+/* TODO: (참고 사이트) https://myhappyman.tistory.com/145 */
+/*
+function () {
+    calendarMaker(("#calendarForm"), new Date());
+}
+
+var nowDate = new Date();
+function calendarMaker(target, date) {
+    if (date == null || date == undefined) {
+        date = new Date();
+    }
+    nowDate = date;
+    if ($(target).length > 0) {
+        var year = nowDate.getFullYear();
+        var month = nowDate.getMonth() + 1;
+        $(target).empty().append(assembly(year, month));
+    } else {
+        console.error("custom_calendar Target is empty!!!");
+        return;
+    }
+
+    var thisMonth = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1);
+    var thisLastDay = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0);
+
+
+    var tag = "<tr>";
+    var cnt = 0;
+    //빈 공백 만들어주기
+    for (i = 0; i < thisMonth.getDay(); i++) {
+        tag += "<td></td>";
+        cnt++;
+    }
+
+    //날짜 채우기
+    for (i = 1; i <= thisLastDay.getDate(); i++) {
+        if (cnt % 7 == 0) { tag += "<tr>"; }
+
+        tag += "<td>" + i + "</td>";
+        cnt++;
+        if (cnt % 7 == 0) {
+            tag += "</tr>";
+        }
+    }
+    $(target).find("#custom_set_date").append(tag);
+    calMoveEvtFn();
+
+    function assembly(year, month) {
+        var calendar_html_code =
+            "<table class='custom_calendar_table'>" +
+            "<colgroup>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "<col style='width:81px'/>" +
+            "</colgroup>" +
+            "<thead class='cal_date'>" +
+            "<th><button type='button' class='prev'><</button></th>" +
+            "<th colspan='5'><p><span>" + year + "</span>년 <span>" + month + "</span>월</p></th>" +
+            "<th><button type='button' class='next'>></button></th>" +
+            "</thead>" +
+            "<thead  class='cal_week'>" +
+            "<th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>" +
+            "</thead>" +
+            "<tbody id='custom_set_date'>" +
+            "</tbody>" +
+            "</table>";
+        return calendar_html_code;
+    }
+
+    function calMoveEvtFn() {
+        //전달 클릭
+        $(".custom_calendar_table").on("click", ".prev", function () {
+            nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth() - 1, nowDate.getDate());
+            calendarMaker($(target), nowDate);
+        });
+        //다음날 클릭
+        $(".custom_calendar_table").on("click", ".next", function () {
+            nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate());
+            calendarMaker($(target), nowDate);
+        });
+        //일자 선택 클릭
+        $(".custom_calendar_table").on("click", "td", function () {
+            $(".custom_calendar_table .select_day").removeClass("select_day");
+            $(this).removeClass("select_day").addClass("select_day");
+        });
+    }
+}*/

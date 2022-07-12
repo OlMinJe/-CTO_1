87960330421364baee6cdd16201a96ac7957b4f8
lmj <123dkan@naver.com>
2022-07-12 18:22:18 +0900

    [imj] mypage 페이지 캘린더 작업 1차 완료(추후에 출석체크 기능 만들기)/ 내정보 - main 페이지 대충 완성

--- src/main/resources/templates/mypage/js/mypage.js
+++ src/main/resources/templates/mypage/js/mypage.js
@@ -55,49 +55,54 @@ const date = new Date();
 const renderCalender = () => {
     document.querySelector('.year_month').innerHTML =  date.getFullYear() +'년 ' + (date.getMonth()+1) + '월';
 
-    const prevLast = new Date(date.getFullYear(), date.getFullYear(), 0);
+    // 지난 달 마지막 Date, 이번달 마지막 DAte
+    const prevLast = new Date(date.getFullYear(), date.getMonth(), 0);
     const thisLast = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
     const PLDate = prevLast.getDate();
     const PLDay = prevLast.getDay();
 
     const TLDate = thisLast.getDate();
-    const TLDDay = thisLast.getDay();
+    const TLDay = thisLast.getDay();
 
+    //Dates 기본 배열
     const prevDates = [];
-    const thisDates = [Array(TLDate + 1).keys()].slice(1)
+    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
     const nextDates = [];
-
-    if(PLDate !== 6) {
-        for(let i = 0; i < PLDate+1 ; i++) {
+    //prevDates 계산
+    if(PLDay !== 6) {
+        for(let i = 0; i < PLDay+1 ; i++) {
             prevDates.unshift(PLDate - i);
         }
     }
-    for(let i = 0; i < 7 - TLDate; i++) { nextDates.push(i); }
+    //nextDates 계산
+    for(let i = 1; i < 7 - TLDate; i++) { nextDates.push(i); }
 
+    //Dates 합치기
     const dates = prevDates.concat(thisDates, nextDates);
     const firstDateIndex = dates.indexOf(1);
     const lastDateIndex = dates.indexOf(TLDate);
 
-
+    //Dates 정리
     dates.forEach((date, i) => {
         const condition = i >= firstDateIndex && i <= lastDateIndex + 1
                           ? 'this'
                           : 'other';
-        dates[i] = '<div class="date"><span class="{condition}">' + date + '</span></div>';
+        dates[i] = '<div class="date"><span class="' + condition + '">' + date + '</span></div>';
     })
 
     document.querySelector('.dates').innerHTML = dates.join('');
 
+    //오늘 날짜 표시
     const today = new Date();
-    if(date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()) {
-        for(let date of document.querySelectorAll('this')) {
-            if(+date.innerHTML === today.getFullYear()) {
-                date.classList.add('today');
-                break;
-            }
+      if (date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear()) {
+        for (let date of document.querySelectorAll('.this')) {
+          if (+date.innerText === today.getDate()) {
+            date.classList.add('today');
+            break;
+          }
         }
-    }
+      }
 };
 
 renderCalender();

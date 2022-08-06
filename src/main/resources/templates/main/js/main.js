8cd92482327f2d78067e47c756d90f1f11de056d
lmj <123dkan@naver.com>
2022-08-06 20:54:46 +0900

    [lmj] 색상 수정/ main 구조 수정/ 내정보 페이지 퍼블

--- src/main/resources/templates/main/js/main.js
+++ src/main/resources/templates/main/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     main_table_active();
 });
 
-
 // mainvisual 스와이퍼 이벤트
 let mainvisual_swiper = new Swiper(".mySwiper", {
     pagination: {
@@ -10,18 +9,110 @@ let mainvisual_swiper = new Swiper(".mySwiper", {
     },
 });
 
+
 // sub 스와이퍼 이벤트
-const sub_swiper = new Swiper('.sub_swiper_wrap .swiper-container', {
+const sub_swiper = new Swiper('.sub_swiper_list .swiper-container', {
     loop: true,
+    loopAdditionalSlides: 1,
+    /*autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },*/
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: 1,
     slideToClickedSlide: true,
-    effect: 'coverflow',
-    coverflowEffect: {
-        rotate: 30,
-        slideShadows: false,
-    },
+    effect: 'coverflow'
 });
+
+
+// sub_swiper 배경 효과
+//https://marshallku.com/web/tips/html5-canvas%EB%A1%9C-%EB%B0%A4%ED%95%98%EB%8A%98-%EA%B7%B8%EB%A6%AC%EA%B8%B0
+const width = window.innerWidth,
+    height = window.innerHeight*(0.8), /*(0.8)*/
+    stars = createStars(width, height, 30),
+    moon = {
+        x: 0,
+        y: height / 2,
+        r: 45
+    },
+    canvas = document.getElementById("nigthSky"),
+    ctx = canvas.getContext("2d");
+let counter = 0,
+    time = 0;
+
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function createStars(width, height, spacing) {
+    const stars = [];
+
+    for (let x = 0; x < width; x += spacing) {
+        for (let y = 0; y < height; y += spacing) {
+            const star = {
+                x: x + random(spacing),
+                y: y + random(spacing),
+                r: Math.random() * 1.5
+            };
+            stars.push(star);
+        }
+    }
+    return stars;
+}
+
+function fillCircle(ctx, x, y, r, fillStyle) {
+    ctx.beginPath(),
+        ctx.fillStyle = fillStyle,
+        ctx.arc(x, y, r, 0, Math.PI * 2),
+        ctx.fill();
+}
+
+function getOpacity(factor) {
+    const opacityIncrement = 0.6 * Math.abs(Math.sin(factor)),
+        opacity = 0.1 + opacityIncrement;
+    return opacity;
+}
+
+function render() {
+    const gradient = ctx.createLinearGradient(0, 0, 0, height);
+    let newX = time / 10 - 45;
+    gradient.addColorStop(0, "#00111e");
+    gradient.addColorStop(1, "#0a2342");
+    //배경 그래디언트
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+    stars.forEach(function (star, i) {
+        const factor = counter * i,
+            x = star.x,
+            y = star.y,
+            opacity = getOpacity(factor),
+            randomColor = Math.floor((Math.random()*360)+1);
+
+        fillCircle(ctx, x, y, star.r, `hsla(${randomColor}, 30%, 80%, ${opacity})`); //별 그리기
+    });
+
+    newX <= width + 90 // window 너비에 달 지름 추가
+        ? (
+            moon.x = newX,
+                moon.y = newX,
+                time += 5
+        ) : time = 0,
+        // 달에 애니메이션 추가
+
+        fillCircle(ctx, moon.x, 90, moon.r, "#f5f3ce"); // 달 그리기
+
+    counter++;
+    requestAnimationFrame(render);
+}
+
+function newY(x) {
+    return Math.pow(x - width / 2, 2) / 9000 + height / 2 + 1
+}
+
+canvas.width = width,
+    canvas.height = height,
+    render();
+
 
 // 커뮤니티 이벤트
 function main_table_active(){
@@ -71,6 +162,7 @@ function mainEditor_table(){
             '</tr>';
     }
 }
+
 
 // fixed 아이콘 hover 이벤트
 let fixed = document.getElementById("fixed");
--- src/main/resources/templates/mypage/js/mypage.js
+++ src/main/resources/templates/mypage/js/mypage.js
@@ -2,19 +2,34 @@ $(document).ready(function(){
 });
 
 /* 모바일 메뉴 */
-function mobile_menu() {
-    var menu = $('.menu_list').css('display');
-    if( menu == 'none') {
-        $('.menu_list').css('display','block');
-    } else {
-        $('.menu_list').css('display','none');
+var windowWidth = window.matchMedia("screen and (max-width: 768px)");
+
+
+if(matchMedia("screen and (max-width: 767px)").matches){
+    function mobile_menu() {
+        var menu = $('#menu_list_01').css('display');
+        if( menu == 'none') {
+            $('#menu_list_01').css('display','block');
+            $('#menu_list_02').css('display','block');
+        } else {
+            $('#menu_list_01').css('display','none');
+            $('#menu_list_02').css('display','none');
+        }
     }
+} else {
+    function mobile_menu() {
+        $('#menu_list_01').css('display','block');
+        $('#menu_list_02').css('display','block');
+    }
+
 }
 
+
 //새고할 때 고정 (작업 필요) https://snupi.tistory.com/195 */
+/*
 window.addEventListener('beforeunload', (event) => {
     // 표준에 따라 기본 동작 방지
     event.preventDefault();
     // Chrome에서는 returnValue 설정이 필요함
     event.returnValue = '';
-});
+});*/

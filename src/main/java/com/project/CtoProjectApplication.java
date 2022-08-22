ce36e072be7a9eda6ced845fc35cbe1413c848c8
jmpark <https://lab.hanium.or.kr/22_IG031/22_ig031.git>
2022-08-22 16:56:58 +0900

    08.22 메인화면 출력 성공

--- src/main/java/com/project/CtoProjectApplication.java
+++ src/main/java/com/project/CtoProjectApplication.java
@@ -1,21 +1,22 @@
 package com.project;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.sql.DataSource;
+
 @SpringBootApplication
+
 public class CtoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CtoProjectApplication.class, args);
-	}
-
-	@GetMapping
-	public String HelloWorld() {
-		return "HelloWorld";
 	}
 
 }

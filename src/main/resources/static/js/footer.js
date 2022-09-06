38b305d0f6e11c5a156336f4bd0e1c9ed54c89af
minjelee <123dkan@naver.com>
2022-09-06 23:05:01 +0900

    [lmj] 헤더 1차 완료(디자인/ 방문자-유저-관리자 별로 출력 다르게 해야됨)/ 푸터 팝업 css 작업 ㅇㄹ(내용 채워야됨 - 개인정보처리방침, 이용약관 내용 알아보기) ++ 헤더, 푸터 스크립트 includeHtml.js 파일로 옮김

--- src/main/resources/static/js/footer.js
+++
@@ -1,9 +0,0 @@
-function onClickEvent01() {
-    document.querySelector('#modal01').style.display = 'block';
-}
-function onClickEvent02() {
-    document.querySelector('#modal02').style.display = 'block';
-}
-function offClickEvent(){
-    document.querySelector('.modal_wrap').style.display = 'none';
-}

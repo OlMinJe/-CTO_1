577111fdd1b38af1908060dfb609fe866a297a6c
minjelee <123dkan@naver.com>
2022-09-21 11:50:26 +0900

    [lmj] 메인에 지도 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -27,6 +27,23 @@ function swiperEvent(){
     });
 }
 
+// 지도
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.577491675481404, 126.9770077164053), // 지도의 중심좌표
+        draggable: false, // 지도를 생성할때 지도 이동 및 확대/축소를 막으려면 draggable: false 옵션을 추가하세요
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 버튼 클릭에 따라 지도 이동 기능을 막거나 풀고 싶은 경우에는 map.setDraggable 함수를 사용합니다
+function setDraggable(draggable) {
+    // 마우스 드래그로 지도 이동 가능여부를 설정합니다
+    map.setDraggable(draggable);
+}
+
+
 // sub_swiper 배경 효과
 function subSwiperBack(){
     //https://marshallku.com/web/tips/html5-canvas%EB%A1%9C-%EB%B0%A4%ED%95%98%EB%8A%98-%EA%B7%B8%EB%A6%AC%EA%B8%B0

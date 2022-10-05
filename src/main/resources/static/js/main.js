874dd48ab2b388f44dd9d260115859d6a95447ed
minjelee <123dkan@naver.com>
2022-10-05 11:39:08 +0900

    [lmj] 명연 ㅇㄹ

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,46 +1,14 @@
 $(document).ready(function () {
     subSwiperBack();
     mainTableActive();
+    saying();
     fixedIcon();
 });
 
-// 짭 todolist
-/*
-let inputBox = document.getElementById('inputField');  // 할 일 입력창
-let addToDo = document.getElementById('addToDo');      // 버튼
-let toDoList = document.getElementById('toDoList');    // 할 일 리스트창
+// 실시간 검색어
 
-addToDo.addEventListener('click', function () {    // 버튼에 클릭 이벤트가 발생하면
-    var list = document.createElement('li');     // html 'li' 태그 만들기
-    var Btn = document.createElement('button'); // 버튼 태그
-    if (!inputBox.value)            // 할 일 입력창에 내용이 입력되지 않으면 alert 발생
-        alert('내용을 입력해 주세요!');
-    else {
-        list.innerHTML = '<input type="checkbox" class="list_check" name="list_check" value="list_check"><label for="list_check" style="margin: 0 10px;">' +
-            inputBox.value + '</label>';  // <li>입력된 할 일</li>
-        toDoList.appendChild(list);       // 할 일 리스트창에 자식으로 붙이기
-        inputBox.value = "";               // 할 일 입력창 초기화
-    }
 
-    list.appendChild(Btn);
-    Btn.innerHTML = "X";
-    Btn.addEventListener("click", deleteBtn);
-
-    // list.addEventListener('click', function(){      // 만들어진 list에 클릭 이벤트가 발생하면 줄 긋기
-    //     list.style.textDecoration = "line-through";
-    // })
-    // list.addEventListener('dblclick', function(){   // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
-    //     toDoList.removeChild(list);
-    // })
-})
-
-function deleteBtn(event) {
-    const removeBtn = event.target.parentElement;
-    removeBtn.remove();
-}
-*/
-
-// 찐 todolist
+// todolist
 const toDoForm = document.querySelector("#A17-todo");
 const toDoInput = document.querySelector("#A17-todo input");
 const toDoList = document.querySelector("#A17-todo-list");
@@ -154,23 +122,6 @@ function swiperEvent() {
     });
 }
 
-// 지도
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new kakao.maps.LatLng(37.577491675481404, 126.9770077164053), // 지도의 중심좌표
-        draggable: false, // 지도를 생성할때 지도 이동 및 확대/축소를 막으려면 draggable: false 옵션을 추가하세요
-        level: 3 // 지도의 확대 레벨
-    };
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-// 버튼 클릭에 따라 지도 이동 기능을 막거나 풀고 싶은 경우에는 map.setDraggable 함수를 사용합니다
-function setDraggable(draggable) {
-    // 마우스 드래그로 지도 이동 가능여부를 설정합니다
-    map.setDraggable(draggable);
-}
-
-
 // sub_swiper 배경 효과
 function subSwiperBack() {
     //https://marshallku.com/web/tips/html5-canvas%EB%A1%9C-%EB%B0%A4%ED%95%98%EB%8A%98-%EA%B7%B8%EB%A6%AC%EA%B8%B0
@@ -262,6 +213,21 @@ function subSwiperBack() {
         render();
 }
 
+// 지도
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.577491675481404, 126.9770077164053), // 지도의 중심좌표
+        draggable: false, // 지도를 생성할때 지도 이동 및 확대/축소를 막으려면 draggable: false 옵션을 추가하세요
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 버튼 클릭에 따라 지도 이동 기능을 막거나 풀고 싶은 경우에는 map.setDraggable 함수를 사용합니다
+function setDraggable(draggable) {
+    // 마우스 드래그로 지도 이동 가능여부를 설정합니다
+    map.setDraggable(draggable);
+}
 
 // 커뮤니티+editor 이벤트
 function mainTableActive() {
@@ -310,6 +276,34 @@ function mainTableActive() {
     mainEditorTable();
     mainCommunityLiActive();
 }
+
+// 명언
+function saying(){
+    const words = ['인생은 가까이서 보면 비극이지만 멀리서 보면 희극이다 - 찰리채플린',
+        '때로는 살아있는 것조차도 용기가 될 때가 있다 - 세네카',
+        '서로를 용서하는 것이야 말로 가장 아름다운 사랑의 모습이다 - 존 셰필드',
+        '자신을 화나게 했던 행동을 다른 이에게 행하지 말라 - 소크라테스',
+        '행동의 가치는 그 행동을 끝까지 이루는 데 있다 - 칭기즈탄',
+        '현명한 자라면 찾아낸 기회보다 더 많은 기회를 만들 것이다 - 프랜시스 베이컨',
+        '성공은 열심히 노력하며 기다리는 사람에게 찾아온다 - 토마스 A. 에디슨',
+        '한 인간의 가치는 그가 무엇을 받을 수 있느냐가 아니라 무엇을 주느냐로 판단된다 - 알버트 아인슈타인',
+        '천재란 자신에게 주어진 일을 하는 재능 있는 사람일 뿐이다 - 토마스 A. 에디슨',
+        '믿음이 부족하기 때문에 도전하길 두려워하는 바, 나는 스스로를 믿는다 - 무하마드 알리',
+        '어디를 가든지 마음을 다해 가라 - 공자',
+        '낮에 꿈꾸는 사람은 밤에만 꿈꾸는 사람에게는 찾아오지 않는 많은 것을 알고 있다 - 에드거 앨런 포',
+        '어떤 일을 하기에 앞서 스스로 그 일에 대한 기대를 가져야 한다 - 마이클 조던',
+        '그 어떤 것에서라도 내적인 도움과 위안을 찾을 수 있다면 그것을 잡아라 - 마하트마 간디',
+        '미래의 가장 좋은 점은 한 번에 하루씩 온다는 것이다 - 에이브러햄 링컨',
+        '미래는 여기 있다. 아직 널리 퍼지지 않았을 뿐이다 - 윌리엄 깁슨',
+        '당신을 만나는 모든 사람이 당신과 헤어질 때는 더 나아지고 더 행복해질 수 있도록 하라 - 마더 테레사',
+        '인간의 감정은 누군가를 만날 때와 헤어질 때 가장 순수하며 가장 빛난다 - 장 폴 리히터',
+        '나는 삶을 변화시키는 아이디어를 항상 책에서 얻었다 - 벨 훅스',
+        '모두를 사랑하되, 몇 사람만 믿으라. 누구에게도 잘못을 저지르지 말라 - 윌리엄 셰익스피어'
+    ];
+    const selected = words[Math.floor(Math.random() * words.length)];
+    document.querySelector('.wiseSaying_con').innerHTML = selected;
+}
+
 
 // fixed 아이콘 hover 이벤트
 function fixedIcon() {
